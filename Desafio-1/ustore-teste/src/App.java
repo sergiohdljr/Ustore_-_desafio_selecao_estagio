0eadc650b1b439fc9ef3e2dd0a5c4b27d1988d1d
sergiohdljr <sergio.tjf1@gmail.com>
2023-08-13 22:57:04 -0300

    exception entrada inválida

--- Desafio-1/ustore-teste/src/App.java
+++ Desafio-1/ustore-teste/src/App.java
@@ -10,35 +10,38 @@ public class App {
         List<Poligono> poligonos = new ArrayList<>();
         double areaTotal = 0;
 
-        while (true) {
-            System.out.print("Informe o número de lados do polígono (3 para triângulo, 4 para quadrado): ");
-            int numLados = scanner.nextInt();
+        try {
+            while (true) {
+                System.out.print("Informe o número de lados do polígono (3 para triângulo, 4 para quadrado): ");
+                int numLados = scanner.nextInt();
 
-            if (numLados == 3 || numLados == 4) {
-                System.out.print("Informe o tamanho do lado do polígono (em cm): ");
-                double tamanhoLado = scanner.nextDouble();
-                Poligono poligono = new Poligono(numLados, tamanhoLado);
-                poligonos.add(poligono);
-                areaTotal += poligono.calcularArea();
-            } else {
-                System.out.println("Número de lados não suportado.");
+                if (numLados == 3 || numLados == 4) {
+                    System.out.print("Informe o tamanho do lado do polígono (em cm): ");
+                    double tamanhoLado = scanner.nextDouble();
+                    Poligono poligono = new Poligono(numLados, tamanhoLado);
+                    poligonos.add(poligono);
+                    areaTotal += poligono.calcularArea();
+                } else {
+                    System.out.println("Número de lados não suportado.");
+                }
+                System.out.print("Se Deseja inserir outro polígono digite SIM :");
+                String continuar = scanner.next();
+                if (!continuar.equalsIgnoreCase("SIM")) {
+                    break;
+                }
             }
+            poligonos.forEach(poligono -> {
+                String nomePoligono = poligono.numLados == 3 ? "Triângulo" : "Quadrado";
+                String areaPoligono = new DecimalFormat("#,##0.00").format(poligono.calcularArea());
+                System.out
+                        .println(nomePoligono + " de lado " + poligono.tamanhoLado + " cm e área " + areaPoligono
+                                + " cm²");
+            });
 
-            System.out.print("Deseja inserir outro polígono? (S/N): ");
-            String continuar = scanner.next();
-            if (!continuar.equalsIgnoreCase("S")) {
-                break;
-            }
+            String areaTotalFormatada = new DecimalFormat("#,##0.00").format(areaTotal);
+            System.out.println("Área total " + areaTotalFormatada + " cm²");
+        } catch (Exception e) {
+            System.out.println("Entrada inválida. Tente novamente.");
         }
-
-        poligonos.forEach(poligono -> {
-            String nomePoligono = poligono.numLados == 3 ? "Triângulo" : "Quadrado";
-            String areaPoligono = new DecimalFormat("#,##0.00").format(poligono.calcularArea());
-            System.out
-                    .println(nomePoligono + " de lado " + poligono.tamanhoLado + " cm e área " + areaPoligono + " cm²");
-        });
-
-        String areaTotalFormatada = new DecimalFormat("#,##0.00").format(areaTotal);
-        System.out.println("Área total " + areaTotalFormatada + " cm²");
     }
 }
